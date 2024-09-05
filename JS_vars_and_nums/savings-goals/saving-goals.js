6700c12c78f59d432d795b2d7fcc6ac955a49d2f
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-05 11:47:44 +0200

    challenge done

---
+++ JS_vars_and_nums/savings-goals/saving-goals.js
@@ -0,0 +1,10 @@
+let startingBalance = 5000;
+let monthlyContribution = 1200;
+const month = 12;
+
+let totalSavings = monthlyContribution * month;
+console.log(
+  "After 12 months of saving, you will have ",
+  totalSavings,
+  " in your account."
+);
