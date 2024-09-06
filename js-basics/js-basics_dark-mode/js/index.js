33386311018d344185dc2c156e2fc0e1082c5f73
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-06 11:11:27 +0200

    dark challenge

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const buttonLight = document.querySelector('[data-js="light-mode-button"]');
+const buttonDark = document.querySelector('[data-js="dark-mode-button"]');
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+
+buttonLight.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+buttonDark.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+buttonToggle.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
