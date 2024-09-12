e375aea1d13b59b797d1de0f397bf6c10f50105b
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-12 12:03:36 +0200

    challenge done

--- js-createElement/js-createelement_toast-messages/js/index.js
+++ js-createElement/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,14 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const message = document.createElement("li");
+  message.classList.add("toast-container__message");
+  message.textContent = "I'm a toast message.";
+  toastContainer.append(message);
+  console.log(message.textContent);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = ``;
 });
