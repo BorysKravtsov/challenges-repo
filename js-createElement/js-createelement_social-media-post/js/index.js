2a69f36aa8aefbbf2e45ac1d5283713938b3055a
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-12 12:45:52 +0200

    idk how to place script beforeDOM

--- js-createElement/js-createelement_social-media-post/js/index.js
+++ js-createElement/js-createelement_social-media-post/js/index.js
@@ -8,5 +8,30 @@ function handleLikeButtonClick(event) {
 const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
 
+const newPost = document.createElement("section");
+newPost.classList.add("post");
+document.body.append(newPost);
+
+const content = document.createElement("p");
+content.classList.add("post__content");
+content.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+newPost.append(content);
+
+const footer = document.createElement("footer");
+footer.classList.add("post__footer");
+newPost.append(footer);
+
+const username = document.createElement("span");
+username.classList.add("post__username");
+username.textContent = "@username";
+footer.append(username);
+
+const button = document.createElement("button");
+button.classList.add("post__button");
+button.textContent = "♥ Like";
+button.dataset.js = "like-button";
+footer.append(button);
+
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
