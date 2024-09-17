94c79a80909eb4ef3e769307627e85b512fbec52
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-17 11:24:18 +0200

    mult-arg done

--- callback-func/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ callback-func/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -4,4 +4,16 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe", "admin");
 }
 
+function showWelcomeMessage(userName, userRole) {
+  console.log(
+    "Welcome " + userName + "! You are logged in now as" + userRole + "."
+  );
+}
+
+handleUserLogin(showWelcomeMessage);
+handleUserLogin((userName, userRole) => {
+  console.log(
+    "Welcome " + userName + "! You are logged in now as" + userRole + "."
+  );
+});
 // The exercise starts here!
