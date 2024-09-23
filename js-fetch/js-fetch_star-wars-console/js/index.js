dae57357826dfe606c209b729cde0712397d9c65
Borys Kravtsov <mamkinboris3@gmail.com>
2024-09-23 12:54:01 +0200

    star-wars console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,20 @@
 console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("API Error: " + response.status);
+    }
 
-function fetchData() {}
+    const eyeColor = await response.json();
+    const characters = eyeColor.results;
+    const r2d2 = characters.find((character) => character.name === "R2-D2");
+    console.log(r2d2.eye_color);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 fetchData();
